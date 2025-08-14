834598075b91b5597c96f9cb50344b1b92393632
cragtok <lidfun@protonmail.com>
2025-08-14 15:04:12 +0600

    refactor: move logo image to layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
+import Image from "next/image";
 import "./globals.css";
 
 const notoSans = Noto_Sans({
@@ -19,7 +20,18 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${notoSans.variable} mx-10 antialiased bg-neutral-100`}>
+            <body
+                className={`${notoSans.variable} mx-10 antialiased bg-neutral-100`}
+            >
+                <div className="mx-auto my-0 flex justify-center mt-15">
+                    <Image
+                        src="/vercel.svg"
+                        className="bg-brand-700 rounded-[8] border-3 border-brand-700"
+                        alt="logo"
+                        width={80}
+                        height={80}
+                    />
+                </div>
                 {children}
             </body>
         </html>
