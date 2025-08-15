4158ff093e51cf7aa6b84a2fd00f6c45e61b788a
cragtok <lidfun@protonmail.com>
2025-08-15 16:59:14 +0600

    feat: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col pt-10 gap-8 items-center">
+            <h1 className="text-brand-700 font-extrabold text-4xl text-center">
+                Page Not Found
+            </h1>
+        </div>
+    );
+}
