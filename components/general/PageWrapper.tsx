3cb4666633cba13ac06e242847a46ddc0cb19f9f
cragtok <lidfun@protonmail.com>
2025-08-18 15:39:52 +0600

    refactor: make pageTitle prop optional for PageWrapper.tsx

--- components/general/PageWrapper.tsx
+++ components/general/PageWrapper.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 interface Props {
     children: React.ReactNode;
-    pageTitle: string;
+    pageTitle?: string;
 }
 
 export function PageWrapper({ children, pageTitle }: Props) {
     return (
         <div className="flex flex-col pt-10 gap-10 items-center text-center max-sm:text-sm pb-10">
-            <h1 className="text-brand-700 font-extrabold text-4xl">
-                {pageTitle}
-            </h1>
+            {pageTitle && (
+                <h1 className="text-brand-700 font-extrabold text-4xl">
+                    {pageTitle}
+                </h1>
+            )}
             {children}
         </div>
     );
