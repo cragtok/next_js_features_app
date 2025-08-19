581d86184158990786795376fb55a6a8f699ed82
cragtok <lidfun@protonmail.com>
2025-08-19 19:49:57 +0600

    refactor: FolderStructureCards to handle multiple files in folder

--- app/routes/FolderStructureCards.tsx
+++ app/routes/FolderStructureCards.tsx
@@ -11,26 +11,46 @@ export default function FolderStructureCards({ folderPath }: Props) {
             {folderPath.map((item, idx) => {
                 const currentLeftPadding = leftPadding;
                 leftPadding += 25;
-                return (
+
+                if (idx < folderPath.length - 1) {
+                    return (
+                        <Card
+                            key={crypto.randomUUID()}
+                            className="bg-neutral-100 rounded-md pt-5 pb-3"
+                        >
+                            <CardHeader>
+                                <CardTitle
+                                    className="flex gap-2 text-accent-500 font-semibold"
+                                    style={{
+                                        paddingLeft: `${currentLeftPadding}px`,
+                                    }}
+                                >
+                                    <span>🗁</span>
+                                    <span>{item}</span>
+                                </CardTitle>
+                            </CardHeader>
+                        </Card>
+                    );
+                }
+
+                return item.split("/").map((filePath) => (
                     <Card
                         key={crypto.randomUUID()}
                         className="bg-neutral-100 rounded-md pt-5 pb-3"
                     >
                         <CardHeader>
                             <CardTitle
-                                className={`flex gap-2 text-accent-500 font-semibold`}
+                                className="flex gap-2 text-accent-500 font-semibold"
                                 style={{
                                     paddingLeft: `${currentLeftPadding}px`,
                                 }}
                             >
-                                <span>
-                                    {idx === folderPath.length - 1 ? "🗐" : "🗁"}
-                                </span>
-                                <span>{item}</span>
+                                <span>🗐</span>
+                                <span>{filePath}</span>
                             </CardTitle>
                         </CardHeader>
                     </Card>
-                );
+                ));
             })}
         </>
     );
