188e2b1abdd6add6bb646bea98620468c7d73625
cragtok <lidfun@protonmail.com>
2025-08-20 19:46:22 +0600

    feat: /streaming/suspense route

---
+++ app/streaming/suspense/DelayedImageComponent.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import ParagraphWrapper from "@/components/general/ParagraphWrapper";
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+interface Props {
+    imagePath: string;
+    componentNumber: number;
+    delaySeconds: number;
+}
+
+export async function DelayedImageComponent({
+    imagePath,
+    componentNumber,
+    delaySeconds,
+}: Props) {
+    await delay(delaySeconds * 1000);
+    return (
+        <Card className="bg-neutral-100 w-full">
+            <CardHeader className="text-brand-700">
+                <CardTitle>Component {componentNumber}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex max-[628px]:flex-col flex-row gap-8">
+                <div className="relative rounded-md overflow-hidden flex-1 max-[628px]:min-h-[100px]">
+                    <Image
+                        src={imagePath}
+                        alt={`Nature Image ${componentNumber}`}
+                        fill={true}
+                        className="object-cover rounded-md"
+                    />
+                </div>
+                <ParagraphWrapper classNameOverride="text-sm flex-2">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Omnis odio id reiciendis. Blanditiis amet doloremque aperiam
+                    voluptatibus magnam aut, accusamus temporibus culpa
+                    voluptatem placeat est magni eum dolore sunt ipsam?
+                </ParagraphWrapper>
+            </CardContent>
+        </Card>
+    );
+}
---
+++ app/streaming/suspense/page.tsx
@@ -0,0 +1,53 @@
+import { PageWrapper } from "@/components/general/PageWrapper";
+import ParagraphWrapper from "@/components/general/ParagraphWrapper";
+import { SectionWrapper } from "@/components/general/SectionWrapper";
+import TextAccentWrapper from "@/components/general/TextAccentWrapper";
+import { DelayedImageComponent } from "./DelayedImageComponent";
+import { Suspense } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+
+export default function Page() {
+    return (
+        <PageWrapper pageTitle="Streaming with Suspense">
+            <SectionWrapper>
+                <ParagraphWrapper>
+                    To demonstrate streaming with{" "}
+                    <TextAccentWrapper>Suspense</TextAccentWrapper>, this
+                    section renders three components that are loaded after each
+                    other with a 2 second gap.
+                </ParagraphWrapper>
+                {[1, 2, 3].map((num) => (
+                    <Suspense key={num} fallback={<LoadingSpinner />}>
+                        <DelayedImageComponent
+                            imagePath={`/nature-image-${num}.jpg`}
+                            componentNumber={num}
+                            delaySeconds={num * 2}
+                        />
+                    </Suspense>
+                ))}
+            </SectionWrapper>
+
+            <SectionWrapper>
+                <ParagraphWrapper>
+                    In Next.js, streaming with Suspense boundaries offers
+                    fine-grained control over how parts of your page are
+                    progressively rendered. By wrapping individual components or
+                    sections of your UI with React&apos;s{" "}
+                    <TextAccentWrapper>Suspense</TextAccentWrapper> component,
+                    you can specify a{" "}
+                    <TextAccentWrapper>fallback</TextAccentWrapper> UI to be
+                    displayed immediately while the wrapped component (often a
+                    Server Component fetching data) is still loading. This
+                    allows the rest of the page to render and stream to the
+                    client without being blocked by slower data fetches,
+                    improving perceived performance and user interactivity. Once
+                    the data for the suspended component is ready, its content
+                    is streamed in, seamlessly replacing the fallback. This
+                    approach is ideal for scenarios where different parts of a
+                    page have varying data loading times, enabling a more
+                    dynamic and responsive user experience.
+                </ParagraphWrapper>
+            </SectionWrapper>
+        </PageWrapper>
+    );
+}
