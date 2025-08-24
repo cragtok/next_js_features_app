d55ad43c4dce591858ab44e2cb003647f6ccca16
cragtok <lidfun@protonmail.com>
2025-08-24 18:16:37 +0600

    feat: change color for loading UI of streaming/loading route

--- app/streaming/loading/loading.tsx
+++ app/streaming/loading/loading.tsx
@@ -4,14 +4,14 @@ export default function Loading() {
     return (
         <PageWrapper pageTitle="Loading...">
             <div className="w-full max-w-lg space-y-3 pt-10">
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-100 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
+                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
             </div>
         </PageWrapper>
     );
