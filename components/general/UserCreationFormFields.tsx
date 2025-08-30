5c7bc657e983b535da243b66eb2f9573a9407d66
cragtok <lidfun@protonmail.com>
2025-08-30 19:47:37 +0600

    feat: visibility toggle for password input in user creation form

--- components/general/UserCreationFormFields.tsx
+++ components/general/UserCreationFormFields.tsx
@@ -2,6 +2,9 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 interface Props {
     displayErrors: Record<string, string>;
@@ -18,6 +21,12 @@ const UserCreationFormFields = ({
     password,
     handleInputChange,
 }: Props) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev);
+    };
+
     return (
         <div className="flex flex-col gap-6">
             <div className="grid gap-2">
@@ -73,15 +82,28 @@ const UserCreationFormFields = ({
                         Password
                     </Label>
                 </div>
-                <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="5 characters minimum"
-                    className="max-[450px]:text-xs"
-                    value={password}
-                    onChange={handleInputChange}
-                />
+                <div className="relative">
+                    <Input
+                        id="password"
+                        name="password"
+                        type={showPassword ? "text" : "password"}
+                        placeholder="5 characters minimum"
+                        className="max-[450px]:text-xs pr-10"
+                        value={password}
+                        onChange={handleInputChange}
+                    />
+                    <Button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute right-2 top-1/2 -translate-y-1/2 p-1 h-auto bg-transparent hover:bg-transparent text-brand-500 hover:text-brand-700"
+                    >
+                        {showPassword ? (
+                            <EyeOffIcon className="h-4 w-4" />
+                        ) : (
+                            <EyeIcon className="h-4 w-4" />
+                        )}
+                    </Button>
+                </div>
                 {displayErrors.password && (
                     <p className="text-status-danger-500 text-sm max-[450px]:text-xs text-left">
                         {displayErrors.password}
