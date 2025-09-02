ebd2903dafaad5f371d874bc4bbf74f37be1314f
cragtok <lidfun@protonmail.com>
2025-09-03 04:02:48 +0600

    feat: update text for /rendering/isr page

--- app/rendering/isr/page.tsx
+++ app/rendering/isr/page.tsx
@@ -16,32 +16,34 @@ async function Page() {
                         Incremental Static Regeneration (ISR)
                     </TextAccentWrapper>
                     , the page is initially statically rendered on the server
-                    during build time. However, unlike a pure static page, an
+                    during build time. However, unlike a purely static page, an
                     ISR page can periodically fetch new data after a specified
                     time period without having to rebuild the entire site.
                 </ParagraphWrapper>
                 <ParagraphWrapper>
-                    ISR can be implemented in a page by exporting a{" "}
+                    ISR can be implemented on a page by exporting a{" "}
                     <TextAccentWrapper>revalidate</TextAccentWrapper> constant
                     that is set to a specified amount of time (known as the{" "}
                     <TextAccentWrapper>revalidation time</TextAccentWrapper>) in
                     seconds. When a request comes in and the cached version of
                     the page is <TextAccentWrapper>stale</TextAccentWrapper>{" "}
                     (older than the specified revalidation time), Next.js serves
-                    the it immediately from the cache. After this time has
+                    the page immediately from the cache. After this time has
                     elapsed, the cache is{" "}
                     <TextAccentWrapper>invalidated</TextAccentWrapper> and a
-                    re-generation of the page occurs in the background on the
-                    server. After it is ready, it replaces the old page in the
-                    cache, and subsequent requests receive the fresh content.
+                    re-generation of the page occurs the server by fetching any
+                    new data. After it is ready, the new page replaces the old
+                    page in the cache, and subsequent requests receive the fresh
+                    content.
                 </ParagraphWrapper>
                 <ParagraphWrapper>
                     ISR combines the performance benefits of static sites (fast
                     initial load, CDN delivery) with the flexibility of dynamic
-                    rendering (fresh data, without full redeploys), making it
+                    rendering (fresh data without full redeploys), making it
                     ideal for content that updates regularly but not on every
                     single user request, such as blog posts, product listings,
-                    or news articles.
+                    or news articles. It is one of the main features of Next.js
+                    which attracts developers to use the framework.
                 </ParagraphWrapper>
             </SectionWrapper>
 
