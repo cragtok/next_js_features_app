57ff1efa3a77e3135100bfb236e88f81e019f8c9
cragtok <lidfun@protonmail.com>
2025-09-02 08:33:07 +0600

    feat: improved styling and functionality for not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,46 @@
+"use client";
+import PageWrapper from "@/components/general/PageWrapper";
+import ParagraphWrapper from "@/components/general/ParagraphWrapper";
+import SectionWrapper from "@/components/general/SectionWrapper";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
 function NotFound() {
+    const router = useRouter();
+
+    const handleGoBack = () => {
+        router.back();
+    };
     return (
-        <div className="flex flex-col pt-10 gap-8 items-center">
-            <h1 className="text-brand-700 font-extrabold text-4xl text-center">
-                Page Not Found
+        <PageWrapper>
+            <div className="mx-auto my-0 flex justify-center">
+                <Image
+                    src="/vercel.svg"
+                    className="bg-brand-700 rounded-[8] border-3 border-brand-700"
+                    alt="logo"
+                    width={80}
+                    height={80}
+                />
+            </div>
+
+            <h1 className="text-status-danger-500 font-extrabold text-4xl text-center">
+                Page Not Found!
             </h1>
-        </div>
+
+            <SectionWrapper classNameOverride="gap-10 max-w-md pt-5">
+                <ParagraphWrapper classNameOverride="text-brand-500 font-bold text-center">
+                    The page you are trying to visit does not exist.
+                </ParagraphWrapper>
+
+                <Button
+                    className="bg-brand-500 hover:bg-brand-700 font-semibold max-w-prose"
+                    onClick={handleGoBack}
+                >
+                    Go Back
+                </Button>
+            </SectionWrapper>
+        </PageWrapper>
     );
 }
 
