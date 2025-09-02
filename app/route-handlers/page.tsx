c76645dd5c2ddab7f49e2d8d48ac6b7902d1cf46
cragtok <lidfun@protonmail.com>
2025-09-03 05:04:53 +0600

    feat: change text for /route-handlers page

--- app/route-handlers/page.tsx
+++ app/route-handlers/page.tsx
@@ -12,24 +12,22 @@ function Page() {
             <SectionWrapper>
                 <ParagraphWrapper>
                     <TextAccentWrapper>Route Handlers</TextAccentWrapper> in
-                    Next.js App Router provide a simple way to create custom
-                    request handlers for specific routes, acting as an API layer
+                    Next.js provide a simple way to create custom HTTP request
+                    handlers for specific routes. They act as an API layer
                     directly within the{" "}
-                    <TextAccentWrapper>app</TextAccentWrapper> directory. They
-                    allow you to build RESTful APIs, handle webhooks, or perform
-                    server-side logic for various HTTP methods ({" "}
+                    <TextAccentWrapper>app</TextAccentWrapper> directory. Using
+                    them, you can build RESTful APIs, handle webhooks, or
+                    perform server-side logic for various HTTP methods ({" "}
                     <TextAccentWrapper>GET</TextAccentWrapper>,{" "}
                     <TextAccentWrapper>POST</TextAccentWrapper>,{" "}
                     <TextAccentWrapper>PUT</TextAccentWrapper>,{" "}
-                    <TextAccentWrapper>DELETE</TextAccentWrapper>, etc.) by
-                    exporting functions with corresponding names (e.g.,{" "}
+                    <TextAccentWrapper>DELETE</TextAccentWrapper>, etc). To
+                    create them, you export functions with corresponding HTTP
+                    method names (e.g.,{" "}
                     <TextAccentWrapper>GET</TextAccentWrapper>,{" "}
                     <TextAccentWrapper>POST</TextAccentWrapper>) from a{" "}
-                    <TextAccentWrapper>route.ts</TextAccentWrapper> file. This
-                    approach offers a more integrated and flexible alternative
-                    to the traditional API routes found in the Pages Router,
-                    enabling you to keep related API logic co-located with your
-                    UI components.
+                    <TextAccentWrapper>route.ts</TextAccentWrapper> file, and
+                    handle any request/reponse logic within them.
                 </ParagraphWrapper>
             </SectionWrapper>
 
@@ -111,13 +109,13 @@ function Page() {
                 <ParagraphWrapper>
                     In this section, you can perform all the{" "}
                     <TextAccentWrapper>CRUD</TextAccentWrapper> operations on
-                    users in the database. There is a list of all the current
-                    users in the database. Each user card has buttons for
-                    editing the user information or deleting it from the
-                    database. There is also a form for creating new users, which
-                    will add a new user to the database and display it in the
-                    list below. All the operations are made using the route
-                    handlers from the API.
+                    users in the database. All of all the current users in the
+                    database rendered here within user cards. Each user card has
+                    buttons for editing a user&apos;s information or deleting it
+                    from the database. There is also a form for creating new
+                    users, which will add a new user to the database and display
+                    it in the list below. All these operations are performed
+                    using the route handlers from the API.
                 </ParagraphWrapper>
 
                 <UsersSection />
