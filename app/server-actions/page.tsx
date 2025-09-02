905d2cb5dfcb85e52cd7364cfdb22261bdf425cc
cragtok <lidfun@protonmail.com>
2025-09-03 04:54:31 +0600

    feat: change text for /server-actions page

--- app/server-actions/page.tsx
+++ app/server-actions/page.tsx
@@ -4,55 +4,58 @@ import SectionWrapper from "@/components/general/SectionWrapper";
 import TextAccentWrapper from "@/components/general/TextAccentWrapper";
 import ServerActionForm from "./ServerActionForm";
 import UsersList from "./UsersList";
-import PathWithBreaks from "@/components/general/PathWithBreaks";
+import { DB_CACHE_TAG } from "@/lib/database/constants";
 
 async function Page() {
     return (
         <PageWrapper pageTitle="Server Actions">
             <ParagraphWrapper>
                 <TextAccentWrapper>Server Actions</TextAccentWrapper> are
-                functions that are executed on the server after a request from a
-                client. To mark a function as a Server Action, place the{" "}
+                functions that are executed on the server as a result of a
+                request from a client. To mark a function as a Server Action,
+                place the{" "}
                 <TextAccentWrapper>&quot;use server&quot;</TextAccentWrapper>{" "}
                 directive at the top of the function. To mark all functions in a
                 file as Server Actions, place the directive at the top of the
                 file. A very common use case for Server Actions is to submit
-                form data from the client.
+                form data from the client to the server.
             </ParagraphWrapper>
 
-            <SectionWrapper sectionTitle="User List">
-                <ParagraphWrapper>
-                    This is a list of users, simulating a data fetch from a mock
-                    database that is located as a file in{" "}
-                    <TextAccentWrapper>
-                        <PathWithBreaks text="data/mockDb.json" />
-                    </TextAccentWrapper>
-                    .
+            <SectionWrapper sectionTitle="Users List">
+                <ParagraphWrapper classNameOverride="text-center">
+                    This is a list of users fetched from a database:
                 </ParagraphWrapper>
                 <UsersList />
                 <ParagraphWrapper>
-                    The users list is fetched within an{" "}
+                    The users are fetched within an{" "}
                     <TextAccentWrapper>unstable_cache</TextAccentWrapper>{" "}
-                    function, since it is meant to simulate a database. If you
-                    are fetching data from a remote API, you can use{" "}
-                    <TextAccentWrapper>fetch</TextAccentWrapper> instead. In
-                    order to identify the data in the cache, it is marked with a{" "}
-                    <TextAccentWrapper>&quot;db-users&quot;</TextAccentWrapper>{" "}
-                    tag. If we invalidate this tag in a Server Action, the list
-                    of users will have to update itself with any new data.
+                    function, since it is meant to run a database query. If you
+                    are fetching data from a remote API, you can use the
+                    <TextAccentWrapper>fetch</TextAccentWrapper> function
+                    instead. In order to identify the data in the cache, it is
+                    marked with a{" "}
+                    <TextAccentWrapper>
+                        &quot;{DB_CACHE_TAG}&quot;
+                    </TextAccentWrapper>{" "}
+                    tag in the{" "}
+                    <TextAccentWrapper>unstable_cache</TextAccentWrapper>{" "}
+                    function. If we invalidate this tag in a Server Action, the
+                    list of users will have to update itself with any new data.
                 </ParagraphWrapper>
             </SectionWrapper>
 
             <SectionWrapper sectionTitle="User Creation Form">
                 <ParagraphWrapper>
-                    This form can be used to create a new user. When the form is
-                    submitted (with valid data), the data is sent to the server,
+                    This form can be used to create a new user in the database.
+                    When the form is submitted, the data is sent to the server,
                     passed to the Server Action, checked for validity, and used
-                    to create a new user. To add the new user to the list above,
+                    to store a new user. To add the new user to the list above,
                     the <TextAccentWrapper>revalidateTag</TextAccentWrapper>{" "}
                     function is called in the Server Action to invalidate the
                     user cache marked with the{" "}
-                    <TextAccentWrapper>&quot;db-users&quot;</TextAccentWrapper>{" "}
+                    <TextAccentWrapper>
+                        &quot;{DB_CACHE_TAG}&quot;
+                    </TextAccentWrapper>{" "}
                     tag. This will cause the users list to be fetched again and
                     thus updated with the newly created user.
                 </ParagraphWrapper>
