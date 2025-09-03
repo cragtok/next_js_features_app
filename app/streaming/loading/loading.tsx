d514a797d7db1a25aaede4b3c1f5a9b9cb913714
cragtok <lidfun@protonmail.com>
2025-09-03 06:19:52 +0600

    feat: use LoadingSpinner in streaming page loading.tsx

--- app/streaming/loading/loading.tsx
+++ app/streaming/loading/loading.tsx
@@ -1,18 +1,10 @@
+import LoadingSpinner from "@/components/general/LoadingSpinner";
 import PageWrapper from "@/components/general/PageWrapper";
 
 function Loading() {
     return (
         <PageWrapper pageTitle="Loading...">
-            <div className="w-full max-w-lg space-y-3 pt-10">
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-                <div className="h-4 bg-brand-50 rounded animate-pulse w-full"></div>
-            </div>
+            <LoadingSpinner />
         </PageWrapper>
     );
 }
