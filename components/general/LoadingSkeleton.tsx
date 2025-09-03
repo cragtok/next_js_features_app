04eb9cdd2be76ac16ec2dc1aa870938ef8c9cd87
cragtok <lidfun@protonmail.com>
2025-09-03 06:13:14 +0600

    feat: LoadingSkeleton component

---
+++ components/general/LoadingSkeleton.tsx
@@ -0,0 +1,20 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface Props {
+    numRows: number;
+}
+
+const LoadingSkeleton = ({ numRows }: Props) => {
+    return (
+        <Card className="bg-neutral-100 rounded-md pt-5 pb-3">
+            <CardContent className="space-y-3">
+                {Array.from({ length: numRows }).map((_, index) => (
+                    <Skeleton key={index} className="h-4 w-full bg-brand-50" />
+                ))}
+            </CardContent>
+        </Card>
+    );
+};
+
+export default LoadingSkeleton;
