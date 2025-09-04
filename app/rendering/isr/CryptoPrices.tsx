ba6fbf4e510f511ac2483129397f82dde5da30f1
cragtok <lidfun@protonmail.com>
2025-09-04 07:40:50 +0600

    feat: add RefreshButton in CryptoPrices

--- app/rendering/isr/CryptoPrices.tsx
+++ app/rendering/isr/CryptoPrices.tsx
@@ -2,31 +2,39 @@ import { CardContent } from "@/components/ui/card";
 import { fetchPrices, CryptoData } from "./apiCall";
 import CryptoPricesCard from "./CryptoPricesCard";
 import CardWrapper from "@/components/general/CardWrapper";
+import RefreshButton from "@/components/general/RefreshButton";
 
 async function CryptoPrices() {
     const prices: CryptoData[] = await fetchPrices();
 
     if (!prices.length) {
         return (
-            <CardWrapper>
-                <CardContent>
-                    <p className="text-status-danger-500 font-semibold">
-                        Failed to load prices. Please try refreshing the page.
-                    </p>
-                </CardContent>
-            </CardWrapper>
+            <>
+                <CardWrapper>
+                    <CardContent>
+                        <p className="text-status-danger-500 font-semibold">
+                            Failed to load prices. Please try refreshing the
+                            page.
+                        </p>
+                    </CardContent>
+                </CardWrapper>
+                <RefreshButton>Refresh Page</RefreshButton>
+            </>
         );
     }
 
     return (
-        <div className="flex flex-col">
-            {prices.map((cryptoData) => (
-                <CryptoPricesCard
-                    key={cryptoData.symbol}
-                    cryptoData={cryptoData}
-                />
-            ))}
-        </div>
+        <>
+            <div className="flex flex-col">
+                {prices.map((cryptoData) => (
+                    <CryptoPricesCard
+                        key={cryptoData.symbol}
+                        cryptoData={cryptoData}
+                    />
+                ))}
+            </div>
+            <RefreshButton>Refresh Page</RefreshButton>
+        </>
     );
 }
 
