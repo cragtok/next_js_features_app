e3c6ce15bb139304706c101ee42cd11fbd5476ad
cragtok <lidfun@protonmail.com>
2025-09-06 05:49:38 +0600

    feat: configure pino log level based on NODE_ENV

--- lib/logging/pino.ts
+++ lib/logging/pino.ts
@@ -1,8 +1,10 @@
 import { pino } from "pino";
+import { serverEnv } from "../env/serverEnv";
+
+const minLogLevel = serverEnv.NODE_ENV === "development" ? "debug" : "info";
 
 const PinoLogger = pino({
-    level: "debug", // DEVELOPMENT
-    // level: "info", // PRODUCTION
+    level: minLogLevel,
 });
 
 export default PinoLogger;
