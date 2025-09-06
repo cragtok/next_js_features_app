4ac81cda0e209f0c16ef474cbffc7046e05981e4
cragtok <lidfun@protonmail.com>
2025-09-06 06:41:02 +0600

    chore: revert to using database handler in seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,29 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
-import { pino } from "pino";
-import Database from "better-sqlite3";
-import { DB_FILE_PATH } from "@/lib/database/constants";
-
-interface User {
-    id: string;
-    username: string;
-    email: string;
-    password: string;
-    createdAt?: string;
-}
-const db = new Database(DB_FILE_PATH);
-db.pragma("journal_mode = WAL");
-
-db.exec(`
-      CREATE TABLE IF NOT EXISTS users (
-        id TEXT PRIMARY KEY,
-        username TEXT NOT NULL UNIQUE,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL,
-        createdAt TEXT DEFAULT CURRENT_TIMESTAMP
-      );
-`);
-
-const scriptLogger = pino({ level: "debug" });
+import { clearDb, seedDb, User } from "@/lib/database/databaseHandler";
+import logger from "@/lib/logging/logger";
 
 const mockUsers: User[] = [
     {
@@ -46,40 +23,21 @@ const mockUsers: User[] = [
     },
 ];
 
-async function clearDb(): Promise<void> {
-    db.prepare("DELETE FROM users").run();
-}
-
-async function seedDb(usersToSeed: User[]): Promise<void> {
-    const insert = db.prepare(
-        "INSERT INTO users (id, username, email, password, createdAt) VALUES(?, ?, ?, ?, ?)"
-    );
-    db.transaction((users) => {
-        for (const user of users) {
-            insert.run(
-                user.id,
-                user.username,
-                user.email,
-                user.password,
-                new Date().toISOString()
-            );
-        }
-    })(usersToSeed);
-}
-
 async function runSeed() {
     try {
-        scriptLogger.info("[runSeed]: Starting database seeding...");
+        logger.info("runSeed", "Starting database seeding...");
         await clearDb();
         await seedDb(mockUsers);
-        scriptLogger.info(
+        logger.info(
+            "runSeed",
+            "Database seeding complete.",
             { numUsers: mockUsers.length },
-            "[runSeed]: Database seeding complete."
         );
     } catch (error) {
-        scriptLogger.fatal(
+        logger.fatal(
+            "runSeed",
+            "Database seeding failed.",
             { error: error as Error },
-            "[runSeed]: Database seeding failed."
         );
         process.exit(1);
     }
