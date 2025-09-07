edfe1fa5fcce50a6a9ac3e807a37c9d670c3ed94
cragtok <lidfun@protonmail.com>
2025-09-07 06:10:48 +0600

    feat: use Pino logger adapter in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import logger from "./lib/logging/logger";
 
 export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
     const response = NextResponse.next();
 
     if (pathname.startsWith("/middleware/ab-testing")) {
+        logger.info("middleware | ab-testing", "AB-test middleware.");
         let abTestGroup = request.cookies.get("ab-test-group")?.value;
 
         if (!abTestGroup) {
             abTestGroup = Math.random() < 0.5 ? "A" : "B";
+            logger.info(
+                "middleware | ab-testing",
+                "Setting ab-test-group cookie..."
+            );
             response.cookies.set("ab-test-group", abTestGroup, {
                 path: "/",
                 maxAge: 5,
             }); // Set for 5 seconds
+            logger.info("middleware | ab-testing", "ab-test-group cookie set.");
         }
         return response;
     }
 
     if (pathname.startsWith("/middleware/log")) {
+        logger.info("middleware | log", "Logging middleware.");
         const ip =
             request.headers.get("x-forwarded-for") ||
             request.headers.get("x-real-ip") ||
@@ -26,31 +34,35 @@ export function middleware(request: NextRequest) {
 
         const serverTime = new Date().toISOString();
 
+        logger.info("middleware | log", "Collecting request headers...");
         const userAgent = request.headers.get("user-agent") || "N/A";
         const country = request.headers.get("x-geo-country");
         const region = request.headers.get("x-geo-region");
         const city = request.headers.get("x-geo-city");
         const timezone = request.headers.get("x-geo-tz");
+        logger.info("middleware | log", "Request headers collected.");
 
-        const requestHeaders = new Headers(request.headers);
-        requestHeaders.set("x-request-ip", ip);
-        requestHeaders.set("x-server-time", serverTime);
-        requestHeaders.set("x-user-agent", userAgent);
+        logger.info("middleware | log", "Setting response headers...");
+        const responseHeaders = new Headers(request.headers);
+        responseHeaders.set("x-request-ip", ip);
+        responseHeaders.set("x-server-time", serverTime);
+        responseHeaders.set("x-user-agent", userAgent);
         if (country) {
-            requestHeaders.set(
+            responseHeaders.set(
                 "x-geo-location",
                 `${city}, ${region}, ${country} (Timezone: ${timezone})`
             );
         } else {
-            requestHeaders.set(
+            responseHeaders.set(
                 "x-geo-location",
                 "Geo-Location headers not detected for path. This usually happens in local development."
             );
         }
+        logger.info("middleware | log", "Response headers set.");
 
         return NextResponse.next({
             request: {
-                headers: requestHeaders,
+                headers: responseHeaders,
             },
         });
     }
