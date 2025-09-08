507c6491ba3debdedd2da42a32b9cde52a2c1d5b
cragtok <lidfun@protonmail.com>
2025-09-08 09:36:42 +0600

    refactor: error handling in fetchJoke function

--- app/rendering/csr/apiCall.ts
+++ app/rendering/csr/apiCall.ts
@@ -1,16 +1,24 @@
 import { clientEnv } from "@/lib/env/clientEnv";
 
 export const fetchJoke = async (): Promise<string> => {
-    const response = await fetch(clientEnv.NEXT_PUBLIC_JOKE_API_URL, {
-        headers: {
-            Accept: "application/json",
-        },
-    });
+    try {
+        const response = await fetch(clientEnv.NEXT_PUBLIC_JOKE_API_URL, {
+            headers: {
+                Accept: "application/json",
+            },
+        });
 
-    if (!response.ok) {
-        throw new Error("Failed to fetch joke");
+        if (!response.ok) {
+            const errorDetail = response.text();
+            throw new Error(
+                `Failed to fetch quote: ${response.status} ${response.statusText} ${errorDetail}`
+            );
+        }
+
+        const data = await response.json();
+        return data.joke;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Could not fetch joke. Please try again later.");
     }
-
-    const data = await response.json();
-    return data.joke;
 };
