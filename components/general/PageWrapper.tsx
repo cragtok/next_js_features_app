eaf948c6ddc2be398d6e283df4231c24ff358589
cragtok <lidfun@protonmail.com>
2025-09-11 05:59:29 +0600

    feat: pass attributes to underlying div of PageWrapper

--- components/general/PageWrapper.tsx
+++ components/general/PageWrapper.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-interface Props {
+interface Props extends React.ComponentPropsWithRef<"div"> {
     children: React.ReactNode;
     pageTitle?: string;
 }
 
-const PageWrapper = ({ children, pageTitle }: Props) => {
+const PageWrapper = ({ children, pageTitle, ...rest }: Props) => {
     return (
-        <div className="flex flex-col pt-10 gap-10 items-center pb-10">
+        <div
+            className="flex flex-col pt-10 gap-10 items-center pb-10"
+            {...rest}
+        >
             {pageTitle && (
                 <h1 className="text-brand-700 font-extrabold text-4xl text-center">
                     {pageTitle}
