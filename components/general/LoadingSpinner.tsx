531b5b05dc260dc8fff15cd8e65e90f3d6278810
cragtok <lidfun@protonmail.com>
2025-09-11 06:40:44 +0600

    feat: add data-test attribute to underlying div of LoadingSpinner

--- components/general/LoadingSpinner.tsx
+++ components/general/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 const LoadingSpinner = () => {
     return (
-        <div className="flex justify-center items-center h-20">
+        <div className="flex justify-center items-center h-20" data-test="loading-spinner">
             <Loader2 className="h-8 w-8 animate-spin text-brand-500" />
         </div>
     );
