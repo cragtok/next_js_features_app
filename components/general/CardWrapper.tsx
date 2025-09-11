19a891306659117fcb8aadb4f42b137e6f5a833d
cragtok <lidfun@protonmail.com>
2025-09-12 02:52:56 +0600

    feat: pass attributes to underlying Card or CardWrapper

--- components/general/CardWrapper.tsx
+++ components/general/CardWrapper.tsx
@@ -1,17 +1,21 @@
 import { Card } from "../ui/card";
 import { cn } from "@/lib/utils";
 
-interface Props {
+interface Props extends React.ComponentPropsWithoutRef<"div"> {
     classNameOverride?: string;
     children: React.ReactNode;
 }
 
-const CardWrapper = ({ classNameOverride, children }: Props) => {
+const CardWrapper = ({ classNameOverride, children, ...rest }: Props) => {
     const defaultStyles =
         "max-w-prose bg-neutral-100 text-center rounded-md pt-5 pb-3 max-[400px]:text-center max-[450px]:text-sm";
 
     const mergedStyles = cn([defaultStyles, classNameOverride]);
-    return <Card className={mergedStyles}>{children}</Card>;
+    return (
+        <Card className={mergedStyles} {...rest}>
+            {children}
+        </Card>
+    );
 };
 
 export default CardWrapper;
