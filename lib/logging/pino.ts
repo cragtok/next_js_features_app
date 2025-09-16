92d5d0310334df7483e5e3e826a2c9477c7caca5
cragtok <lidfun@protonmail.com>
2025-09-17 05:55:45 +0600

    feat: set higher min log level in testing environments

--- lib/logging/pino.ts
+++ lib/logging/pino.ts
@@ -1,7 +1,15 @@
 import { pino } from "pino";
 import type { Logger as PinoLoggerType } from "pino";
 
-const minLogLevel = process.env.NODE_ENV === "development" ? "debug" : "info";
+let minLogLevel;
+
+if (process.env.NODE_ENV === "development") {
+    minLogLevel = "debug";
+} else if (process.env.NODE_ENV === "production") {
+    minLogLevel = "info";
+} else {
+    minLogLevel = "fatal";
+}
 
 const PinoLogger = pino({
     level: minLogLevel,
