ae758b60a24f3d2e40508714525a89688b91ffac
cragtok <lidfun@protonmail.com>
2025-10-05 20:55:59 +0600

    feat: pass props to LinkWrapper component

--- components/general/LinkWrapper.tsx
+++ components/general/LinkWrapper.tsx
@@ -2,17 +2,17 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 
-interface Props {
+interface Props extends React.ComponentPropsWithRef<"a"> {
     classNameOverride?: string;
     children: React.ReactNode;
     href: string;
 }
-const LinkWrapper = ({ classNameOverride, children, href }: Props) => {
+const LinkWrapper = ({ classNameOverride, children, href, ...rest }: Props) => {
     const defaultStyles =
         "text-accent-500 font-semibold whitespace-normal break-words inline-block underline hover:text-accent-700";
     const mergedStyles = cn([defaultStyles, classNameOverride]);
     return (
-        <Link href={href} className={mergedStyles}>
+        <Link href={href} className={mergedStyles} {...rest}>
             {children}
         </Link>
     );
