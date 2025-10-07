a9e8dc7b5a40de6230609ee9065b6a24f2224c3b
cragtok <lidfun@protonmail.com>
2025-10-07 17:49:11 +0600

    chore: add turso environment variables to serverEnv file

--- lib/env/serverEnv.ts
+++ lib/env/serverEnv.ts
@@ -11,6 +11,8 @@ const serverSchema = z.object({
         .string("TWELVE_DATA_API_KEY must be a valid string")
         .min(1),
     DOMAIN_URL: z.url("DOMAIN_URL must be a valid URL"),
+    TURSO_DATABASE_URL: z.url("TURSO_DATABASE_URL must be a valid URL"),
+    TURSO_AUTH_TOKEN: z.string("TURSO_AUTH_TOKEN must be a valid string"),
 });
 
 export const serverEnv = processEnv(serverSchema, {
@@ -18,4 +20,6 @@ export const serverEnv = processEnv(serverSchema, {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     TWELVE_DATA_API_KEY: process.env.TWELVE_DATA_API_KEY,
     DOMAIN_URL: process.env.DOMAIN_URL,
+    TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
+    TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
 });
